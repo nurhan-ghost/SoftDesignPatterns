121f13fc9d2a4e111e70229a5299d8f690f70efe
nurhan-ghost <turganbeknurhan13@gmail.com>
2025-09-10 14:07:24 +0500

    Delete Assignment1 directory

--- Assignment1/Builder.java
+++
@@ -1,8 +0,0 @@
-package Assignment1;
-public interface Builder {
-    void reset();
-    void setScreen(String type);
-    void setProcessor(String processor);
-    void setBattery(int capacity);
-    void setCamera(int megapixels); 
-}
